93050cea908c47682dd81b9799bb2e6889b8b876
InternetNinjacy <45226145+InternetNinjacy@users.noreply.github.com>
2026-09-21 21:57:32 -0400

    Reserve Fuchsia Gym trainer class and picture IDs

--- include/constants/trainers.h
+++ include/constants/trainers.h
@@ -167,6 +167,12 @@
 #define TRAINER_PIC_LADY                  146
 #define TRAINER_PIC_PAINTER               147
 
+// Pokémon: Sam Edition
+#define TRAINER_PIC_BAZ                   148
+#define TRAINER_PIC_BUSHRANGER_M          149
+#define TRAINER_PIC_BUSHRANGER_F          150
+#define TRAINER_PIC_SATOSHI               151
+
 // The player back pics are assumed to alternate according to the gender values (MALE/FEMALE)
 #define TRAINER_BACK_PIC_RED                    0
 #define TRAINER_BACK_PIC_LEAF                   1
@@ -287,6 +293,7 @@
 #define TRAINER_CLASS_RUIN_MANIAC         104
 #define TRAINER_CLASS_LADY                105
 #define TRAINER_CLASS_PAINTER             106
+#define TRAINER_CLASS_BUSHRANGER          107
 
 #define FACILITY_CLASS_AQUA_LEADER_ARCHIE     0
 #define FACILITY_CLASS_AQUA_GRUNT_M           1
