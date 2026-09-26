ac213c3276fb2ea9ffd8a62f01c702de2ffe8b81
InternetNinjacy <45226145+InternetNinjacy@users.noreply.github.com>
2026-09-26 11:09:03 -0400

    Gym2: add Keahi overworld frame table

--- src/data/object_events/object_event_pic_tables.h
+++ src/data/object_events/object_event_pic_tables.h
@@ -1764,3 +1764,10 @@ static const struct SpriteFrameImage sPicTable_Lehua[] = {
     overworld_frame(gObjectEventPic_Lehua, 2, 4, 2),
     overworld_frame(gObjectEventPic_Lehua, 2, 4, 2),
 };
+
+static const struct SpriteFrameImage sPicTable_Keahi[] = {
+    overworld_frame(gObjectEventPic_Keahi, 2, 4, 0),
+    overworld_frame(gObjectEventPic_Keahi, 2, 4, 1),
+    overworld_frame(gObjectEventPic_Keahi, 2, 4, 2),
+    overworld_frame(gObjectEventPic_Keahi, 2, 4, 3),
+};
