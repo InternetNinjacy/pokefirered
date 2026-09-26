1eec9a1b40df37a517ea4bd6f5b3f2ca2272ab00
InternetNinjacy <45226145+InternetNinjacy@users.noreply.github.com>
2026-09-26 11:31:00 -0400

    Gym2: add Leilani object graphics info

--- src/data/object_events/object_event_graphics_info.h
+++ src/data/object_events/object_event_graphics_info.h
@@ -2961,3 +2961,22 @@ const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_Keahi = {
     .images = sPicTable_Keahi,
     .affineAnims = gDummySpriteAffineAnimTable,
 };
+
+const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_Leilani = {
+    .tileTag = TAG_NONE,
+    .paletteTag = OBJ_EVENT_PAL_TAG_LEILANI,
+    .reflectionPaletteTag = OBJ_EVENT_PAL_TAG_NONE,
+    .size = 256,
+    .width = 16,
+    .height = 32,
+    .paletteSlot = PALSLOT_NPC_1,
+    .shadowSize = SHADOW_SIZE_M,
+    .inanimate = FALSE,
+    .disableReflectionPaletteLoad = TRUE,
+    .tracks = TRACKS_FOOT,
+    .oam = &gObjectEventBaseOam_16x32,
+    .subspriteTables = gObjectEventSpriteOamTables_16x32,
+    .anims = sAnimTable_Stationary4Facing,
+    .images = sPicTable_Leilani,
+    .affineAnims = gDummySpriteAffineAnimTable,
+};
