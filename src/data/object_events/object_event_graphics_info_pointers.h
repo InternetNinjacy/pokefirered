a70b723f5679b357dc2bb50c5e7b0acd26d54758
InternetNinjacy <45226145+InternetNinjacy@users.noreply.github.com>
2026-09-26 11:09:16 -0400

    Gym2: bind OBJ_EVENT_GFX_KEAHI

--- src/data/object_events/object_event_graphics_info_pointers.h
+++ src/data/object_events/object_event_graphics_info_pointers.h
@@ -151,6 +151,7 @@ const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_DeoxysA;
 const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_DeoxysN;
 const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_SSAnne;
 const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_Lehua;
+const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_Keahi;
 
 
 const struct ObjectEventGraphicsInfo *const gObjectEventGraphicsInfoPointers[NUM_OBJ_EVENT_GFX] = {
@@ -307,4 +308,5 @@ const struct ObjectEventGraphicsInfo *const gObjectEventGraphicsInfoPointers[NUM
     [OBJ_EVENT_GFX_DEOXYS_N]                 = &gObjectEventGraphicsInfo_DeoxysN,
     [OBJ_EVENT_GFX_SS_ANNE]                  = &gObjectEventGraphicsInfo_SSAnne,
     [OBJ_EVENT_GFX_LEHUA]                    = &gObjectEventGraphicsInfo_Lehua,
+    [OBJ_EVENT_GFX_KEAHI]                    = &gObjectEventGraphicsInfo_Keahi,
 };
