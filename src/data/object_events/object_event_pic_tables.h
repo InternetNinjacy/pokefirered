77597da4dc6a47110102ebcfb433045555c4302e
InternetNinjacy <45226145+InternetNinjacy@users.noreply.github.com>
2026-09-26 10:44:58 -0400

    Gym2: add Lehua overworld frame table

--- src/data/object_events/object_event_pic_tables.h
+++ src/data/object_events/object_event_pic_tables.h
@@ -1752,3 +1752,15 @@ static const struct SpriteFrameImage sPicTable_Mom[] = {
     overworld_frame(gObjectEventPic_Mom, 2, 4, 2),
     overworld_frame(gObjectEventPic_Mom, 2, 4, 2),
 };
+
+static const struct SpriteFrameImage sPicTable_Lehua[] = {
+    overworld_frame(gObjectEventPic_Lehua, 2, 4, 0),
+    overworld_frame(gObjectEventPic_Lehua, 2, 4, 1),
+    overworld_frame(gObjectEventPic_Lehua, 2, 4, 2),
+    overworld_frame(gObjectEventPic_Lehua, 2, 4, 0),
+    overworld_frame(gObjectEventPic_Lehua, 2, 4, 0),
+    overworld_frame(gObjectEventPic_Lehua, 2, 4, 1),
+    overworld_frame(gObjectEventPic_Lehua, 2, 4, 1),
+    overworld_frame(gObjectEventPic_Lehua, 2, 4, 2),
+    overworld_frame(gObjectEventPic_Lehua, 2, 4, 2),
+};
