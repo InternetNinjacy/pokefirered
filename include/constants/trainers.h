efa1c8fb403e241102072ea27a0edb1e2df192f8
InternetNinjacy <45226145+InternetNinjacy@users.noreply.github.com>
2026-09-25 20:59:15 -0400

    Cerulean Gym: reserve trainers.h symbols

--- include/constants/trainers.h
+++ include/constants/trainers.h
@@ -166,6 +166,10 @@
 #define TRAINER_PIC_RUIN_MANIAC           145
 #define TRAINER_PIC_LADY                  146
 #define TRAINER_PIC_PAINTER               147
+#define TRAINER_PIC_LEILANI               148
+#define TRAINER_PIC_LEHUA                 149
+#define TRAINER_PIC_KEAHI                 150
+#define TRAINER_PIC_SATOSHI               151
 
 // The player back pics are assumed to alternate according to the gender values (MALE/FEMALE)
 #define TRAINER_BACK_PIC_RED                    0
@@ -287,6 +291,7 @@
 #define TRAINER_CLASS_RUIN_MANIAC         104
 #define TRAINER_CLASS_LADY                105
 #define TRAINER_CLASS_PAINTER             106
+#define TRAINER_CLASS_FIRE_DANCER         107
 
 #define FACILITY_CLASS_AQUA_LEADER_ARCHIE     0
 #define FACILITY_CLASS_AQUA_GRUNT_M           1
