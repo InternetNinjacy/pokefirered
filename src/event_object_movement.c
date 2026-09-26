89b68b2cfeac17193d3f22a2e1ecfcb9ae90b671
InternetNinjacy <45226145+InternetNinjacy@users.noreply.github.com>
2026-09-26 11:31:05 -0400

    Gym2: register Leilani overworld palette

--- src/event_object_movement.c
+++ src/event_object_movement.c
@@ -470,6 +470,7 @@ static const u8 gInitialMovementTypeFacingDirections[MOVEMENT_TYPES_COUNT] = {
 #define OBJ_EVENT_PAL_TAG_RS_SUBMARINE_SHADOW         0x111B
 #define OBJ_EVENT_PAL_TAG_LEHUA                       0x111C
 #define OBJ_EVENT_PAL_TAG_KEAHI                       0x111D
+#define OBJ_EVENT_PAL_TAG_LEILANI                     0x111E
 #define OBJ_EVENT_PAL_TAG_NONE                        0x11FF
 
 #include "data/object_events/object_event_graphics_info_pointers.h"
@@ -501,6 +502,7 @@ static const struct SpritePalette sObjectEventSpritePalettes[] = {
     {gObjectEventPal_Seagallop,               OBJ_EVENT_PAL_TAG_SEAGALLOP},
     {gObjectEventPal_Lehua,                   OBJ_EVENT_PAL_TAG_LEHUA},
     {gObjectEventPal_Keahi,                   OBJ_EVENT_PAL_TAG_KEAHI},
+    {gObjectEventPal_Leilani,                 OBJ_EVENT_PAL_TAG_LEILANI},
     {},
 };
 
